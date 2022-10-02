c638624eefa0a91fde9e77f9283df927448870bb
Serhii <writetosergiiu@gmail.com>
2022-10-02 13:23:10 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*our application's code*/
+#include <iostream>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+int main(){
+
+
+    spdlog::info("Hello world!");
+    
+    //log message to file
+    
+    try {
+        auto mylogger = spdlog::basic_logger_mt("mylogger", "logs.txt");
+        mylogger->info("Hello world!");
+    
+    } catch(const spdlog::spdlog_ex &ex) {
+    
+        std::cout << ex.what() <<std::endl;
+    }
+
+}
